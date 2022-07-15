45ec3e3af01d5d5cf4344acac246cdc62a2e5565
MuhEid96 <moh.eid.1996@gmail.com>
2022-07-15 17:26:34 +0200

    added footer component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+
 const Home = () => {
-  return (
-    <div>
-      <h1>Hello home Component!</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello home Component!</h1>
+        </div>
+    );
 };
 
 export default Home;
