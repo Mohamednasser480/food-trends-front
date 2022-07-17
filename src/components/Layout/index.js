330d14c27d67fbc2d3c9d340777d8b494cd7eff5
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-17 20:26:15 +0200

    feat: Create Category Section and Add it to Home.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,6 @@
 import Layout from "./Layout";
 import Footer from "./Footer";
 import Slider from "./Slider";
+import CategoriesSection from "./Categories/CategoriesSection";
 export default Layout;
-export { Footer, Slider };
+export { Footer, Slider, CategoriesSection };
