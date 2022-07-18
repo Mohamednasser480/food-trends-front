1f9259df6fa40817c1acfbe3b28d74b944aad9f6
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-18 23:39:39 +0200

    feat : Create Tooltips for Product Icons

--- src/components/FeatureProducts/ProductIcon.js
+++ src/components/FeatureProducts/ProductIcon.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ProductIcon(props) {
   return (
-    <div className="product-icon group-one flex h-12 w-12 cursor-pointer items-center justify-center rounded-full  border bg-white shadow-xl transition-all duration-300 hover:bg-black hover:border-black">
+    <div className="product-icon group-one flex h-12 w-12 cursor-pointer items-center justify-center rounded-full  border bg-white shadow-xl transition-all duration-300 hover:bg-black hover:border-black" data-tip={props.tooltip}>
       {props.children}
     </div>
   );
--- src/components/FeatureProducts/ProductIcons.js
+++ src/components/FeatureProducts/ProductIcons.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactTooltip from 'react-tooltip';
 import { AiOutlineShopping, AiOutlineStar, AiOutlineEye } from "react-icons/ai";
 import ProductIcon from "./ProductIcon";
 import "./ProductIcons.css";
@@ -6,15 +7,19 @@ import "./ProductIcons.css";
 export default function ProductIcons(props) {
   return (
     <div className={props.className}>
-      <ProductIcon>
+      <ProductIcon tooltip="Add to Wishlist">
         <AiOutlineStar size={25} className={"text-black transition-all"} />
       </ProductIcon>
-      <ProductIcon>
+      <ProductIcon tooltip="Quick view">
         <AiOutlineEye size={25} className={"text-black transition-all"} />
       </ProductIcon>
-      <ProductIcon>
+      <ProductIcon tooltip="Add to Cart">
         <AiOutlineShopping size={25} className={"text-black transition-all"} />
       </ProductIcon>
+
+      {/* Iniliaze Tooltip */}
+      <ReactTooltip effect="solid" className="!font-satoshi !px-2 !py-1 !font-medium " />
+
     </div>
   );
 }
