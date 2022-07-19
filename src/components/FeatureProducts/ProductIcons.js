e6d10df1421339343da8fd024c38c37b5fbd499a
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-19 18:30:47 +0200

    feat: Add Modal To Products

--- src/components/FeatureProducts/ProductIcons.js
+++ src/components/FeatureProducts/ProductIcons.js
@@ -1,25 +1,51 @@
-import React from "react";
-import ReactTooltip from 'react-tooltip';
+import React, { useState } from "react";
+import ReactTooltip from "react-tooltip";
 import { AiOutlineShopping, AiOutlineStar, AiOutlineEye } from "react-icons/ai";
 import ProductIcon from "./ProductIcon";
 import "./ProductIcons.css";
+import { Modal } from "../UI";
 
 export default function ProductIcons(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  function toggleModal(e) {
+    // To stop clicking on Children
+    if (e.target !== e.currentTarget) return;
+    setShowModal(!showModal);
+  }
+
   return (
     <div className={props.className}>
       <ProductIcon tooltip="Add to Wishlist">
         <AiOutlineStar size={25} className={"text-black transition-all"} />
       </ProductIcon>
-      <ProductIcon tooltip="Quick view">
-        <AiOutlineEye size={25} className={"text-black transition-all"} />
+
+      <ProductIcon tooltip="Quick view" onClickHandler={toggleModal}>
+        <AiOutlineEye
+          size={25}
+          className={"text-black transition-all"}
+          onClick={toggleModal}
+        />
+        {showModal && (
+          <Modal
+            toggleModal={toggleModal}
+            className="h-1/2 min-w-[970px]   cursor-default bg-white  p-5"
+            effect="fade"
+          >
+            <h4>Hey</h4>
+          </Modal>
+        )}
       </ProductIcon>
+
       <ProductIcon tooltip="Add to Cart">
         <AiOutlineShopping size={25} className={"text-black transition-all"} />
       </ProductIcon>
 
       {/* Iniliaze Tooltip */}
-      <ReactTooltip effect="solid" className="!font-satoshi !px-2 !py-1 !font-medium !w-fit" />
-
+      <ReactTooltip
+        effect="solid"
+        className="!w-fit !px-2 !py-1 !font-satoshi !font-medium"
+      />
     </div>
   );
 }
