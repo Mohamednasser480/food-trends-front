e6368521704ec9baea3fbee091f2adde62faa0a0
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-19 18:15:25 +0200

    style: Create Portal for Modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,32 +1,32 @@
-import React, { useState } from 'react'
-import AOS from 'aos';
+import React, { useState } from "react";
+import AOS from "aos";
 import "aos/dist/aos.css";
+import { createPortal } from "react-dom";
 
 AOS.init();
 
 export default function Modal(props) {
-    let directionClass = "justify-start";
-    let animationSide = "slide-right";
+  let directionClass = "justify-center";
 
-    if (props.direction == "right") {
-        directionClass = "justify-end"; 
-        animationSide = "slide-left";
-    }
+  if (props.direction == "right") {
+    directionClass = "justify-end";
+  }
+  if (props.direction == "left") {
+    directionClass = "justify-start";
+  }
 
-    return (
-
-        <>
-            {
-                <div
-                    className={`fixed  left-0 top-0 z-50 bg-[#0000006d] w-screen h-screen cursor-crosshair flex ${directionClass} me-5`}
-                    onClick={props.toggleModal}
-                    data-aos="fade"
-                >
-                    <div className='bg-white w-[410px] h-full p-5 max-w-[90%] cursor-default' data-aos={animationSide}>
-                        {props.children}
-                    </div>
-                </div>
-            }
-        </>
-    )
+  return (
+    <>
+      {createPortal(
+        <div
+          className={`fixed left-0 top-0 z-50 flex h-screen w-screen cursor-crosshair items-center bg-[#0000006d] ${directionClass} me-5`}
+          onClick={props.toggleModal}
+          data-aos="fade"
+        >
+          {props.children}
+        </div>,
+        document.getElementById("modals")
+      )}
+    </>
+  );
 }
