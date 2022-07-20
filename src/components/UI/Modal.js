fdc559ac317a2d9cadbc8f491618d26e7507a7e9
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-20 16:30:10 +0200

    style: Change Modal Effect

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -19,11 +19,11 @@ export default function Modal(props) {
     <>
       {createPortal(
         <div
-          className={`fixed left-0 top-0 z-50 flex h-screen w-screen cursor-crosshair items-center bg-[#0000006d] ${directionClass} me-5`}
+          className={`fixed left-0 top-0 z-50 flex h-screen w-screen cursor-[crosshair] items-center bg-[#0000006d] ${directionClass} me-5`}
           onClick={props.toggleModal}
           data-aos="fade"
         >
-          <div className={props.className} data-aos={props.modalEffect}>
+          <div className={props.className} data-aos={props.effect}>
             <div className="flex flex-col">
               <span className="text-base-400 cursor-pointer w-fit self-end text-xl mr-4" onClick={props.toggleModal} >X</span>
               {props.children}</div>
