a686d47ebc1fd58e4754abab1339b18c97ddbc3f
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-21 14:44:30 +0200

    feat: Add React Rating Component

--- src/components/UI/ProductRating.js
+++ src/components/UI/ProductRating.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Rating from "react-rating";
+import { AiFillStar } from "react-icons/ai";
 
 export default function ProductRating(props) {
   return (
-      <h3>Rating</h3>
+    <Rating
+      readonly
+      initialRating={props.rating}
+      emptySymbol={<AiFillStar color="#b7b7b7" />}
+      fullSymbol={<AiFillStar color="#EEB31A" />}
+    />
   );
 }
