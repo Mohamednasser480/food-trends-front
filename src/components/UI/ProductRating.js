dfee9967d1e5521499dd76fe8ba354c3b2723d26
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-21 14:42:34 +0200

    style: remove react stars components

--- src/components/UI/ProductRating.js
+++ src/components/UI/ProductRating.js
@@ -1,15 +1,7 @@
-import ReactStars from "react-stars";
 import React from "react";
 
 export default function ProductRating(props) {
   return (
-    <ReactStars
-      count={5}
-      size={24}
-      edit={false}
-      color2={"#EEB31A"}
-      color1={"#7e7e7e"}
-      value={props.rating}
-    />
+      <h3>Rating</h3>
   );
 }
