2309e2153d41bddaa6f683d8b2e0e9d4dc4d46fd
Eslam Ali <preslamali@hotmail.com>
2022-07-21 22:11:37 +0200

    feat: hide/show "View All" Nav

--- src/components/UI/SectionTitle.js
+++ src/components/UI/SectionTitle.js
@@ -1,23 +1,30 @@
 import React from "react";
 import { Typography } from "../UI";
 import { Link } from "react-router-dom";
-import {BsArrowRight} from "react-icons/bs"
+import { BsArrowRight } from "react-icons/bs";
 
-export default function SectionTitle({text,navigateTo}) {
+export default function SectionTitle({ text, navigateTo }) {
   return (
-    <div className="flex items-center flex-col sm:flex-row  sm:justify-between">
+    <div className="flex flex-col items-center sm:flex-row  sm:justify-between">
       <Typography
         component={"h3"}
-        className={" !tracking-normal text-primary "}
+        className={" !tracking-normal text-primary"}
       >
         {text}
       </Typography>
 
-      <Link to={navigateTo}
-      className="flex items-center gap-2 uppercase font-semibold tracking-wide text-primary hover:text-secondary group font-satoshi">
-        View All
-        <BsArrowRight className="group-hover:-ml-1 transition-all duration-300" size={20} />
-      </Link>
+      {navigateTo && (
+        <Link
+          to={navigateTo}
+          className="group flex items-center gap-2 font-satoshi font-semibold uppercase tracking-wide text-primary hover:text-secondary"
+        >
+          View All
+          <BsArrowRight
+            className="transition-all duration-300 group-hover:-ml-1"
+            size={20}
+          />
+        </Link>
+      )}
     </div>
   );
 }
