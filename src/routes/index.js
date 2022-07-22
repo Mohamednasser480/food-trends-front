ece9ebd2e3068b9c50367136def58fc0be61422b
MuhEid96 <moh.eid.1996@gmail.com>
2022-07-23 00:13:20 +0200

    Feature/user-account: fix shop import bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
-import Home from "./Home";
-import ContactUs from "./ContactUs";
-import Shop from "./Shop";
-import Cart from "./Cart";
-import AboutUs from "./AboutUs";
+import Home from './Home';
+import ContactUs from './ContactUs';
+// import Shop from './Shop';
+import Cart from './Cart';
+import AboutUs from './AboutUs';
 
-export { Home, ContactUs, Shop, Cart, AboutUs };
+export { Home, ContactUs, /*Shop,*/ Cart, AboutUs };
