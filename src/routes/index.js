545eb441790fffbe0cf481f5b62132d8193977a8
Eslam Ali <preslamali@hotmail.com>
2022-07-23 22:03:34 +0200

    bugfix: delete shop component (not found)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import Home from "./Home";
 import ContactUs from "./ContactUs";
-import Shop from "./Shop";
 import Cart from "./Cart";
 import AboutUs from "./AboutUs";
 
-export { Home, ContactUs, Shop, Cart, AboutUs };
+export { Home, ContactUs, Cart, AboutUs };
