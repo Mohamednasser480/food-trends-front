59fc7053a1327ff0d5ae7b33464556996e9c784e
MuhEid96 <moh.eid.1996@gmail.com>
2022-07-24 15:49:49 +0200

    Feature/about-team: add three team members photos

--- src/components/AboutUs/OurTeam.js
+++ src/components/AboutUs/OurTeam.js
@@ -6,25 +6,25 @@ const team = [
     id: 1,
     name: 'Muhammed Eid',
     job: 'Frontend Developer',
-    image: '/sisi.jpg',
+    image: '/eid.jpeg',
   },
   {
     id: 2,
     name: 'Muhammed Nasser',
     job: 'Backend Developer',
-    image: '/sisi.jpg',
+    image: '/nasser.jpeg',
   },
   {
     id: 3,
     name: 'Eslam Ali',
-    job: 'Team Leader',
+    job: 'Team Lead',
     image: '/sisi.jpg',
   },
   {
     id: 4,
     name: 'Mahmoud Fahiem',
     job: 'DevOps Engineer',
-    image: '/sisi.jpg',
+    image: '/fahiem.jpeg',
   },
   {
     id: 5,
@@ -35,18 +35,21 @@ const team = [
 ];
 export default function OurTeam() {
   return (
-    <div className="container my-40 grid grid-cols-5 gap-2 py-10">
-      {team.map((member) => {
-        return (
-          <div key={member.id} className="text-center">
-            <img src={require('../../assets' + member.image)} alt="" />
-            <Typography component={'h6'} className="pt-8">
-              {member.name}
-            </Typography>
-            <Typography component={'body2'}>{member.job}</Typography>
-          </div>
-        );
-      })}
+    <div className="container">
+      <Typography component={'h3'}>Our Team</Typography>
+      <div className=" mb-40 grid grid-cols-5 gap-2 py-10">
+        {team.map((member) => {
+          return (
+            <div key={member.id} className="text-center">
+              <img src={require('../../assets' + member.image)} alt="" className="h-2/3" />
+              <Typography component={'h6'} className="pt-8">
+                {member.name}
+              </Typography>
+              <Typography component={'body2'}>{member.job}</Typography>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
