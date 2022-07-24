b9139277f47477b230072984335590577899f18f
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-24 23:32:05 +0200

    style: change container styling

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import {ImageSection,ProductDetails} from "./index"
-export default function Product({id}) {
-
+import React from "react";
+import { ImageSection, ProductDetails } from "./index";
+export default function Product({ id }) {
+  // Fetch Item Here and send each Details to Components
   return (
-    <div className='container flex flex-wrap'>
-      <ImageSection/>
-      <ProductDetails/>
+    <div className="container flex flex-wrap justify-center gap-2 py-4">
+      <ImageSection />
+      <ProductDetails />
     </div>
-  )
+  );
 }
