c8c94b8706face387176f7017bffa52b8617cbfe
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-30 17:24:52 +0200

    feat: Add Quickview Product

--- src/components/FeatureProducts/QuickviewProduct.js
+++ src/components/FeatureProducts/QuickviewProduct.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
-export default function QuickviewProduct() {
+import React from "react";
+import { Slider, ProductDetails } from "../Product/index";
+export default function QuickviewProduct(props) {
+  console.log(props);
   return (
-    <div>
-      <h4>Product Component Goes Here.</h4>
+    <div className=" flex flex-wrap items-center justify-center">
+      <Slider className="hidden md:block md:w-5/12" />
+      <ProductDetails miny={true} className="w-full gap-2 md:w-7/12" />
     </div>
-  )
+  );
 }
