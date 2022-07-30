3bd7a5b48dade468f41f5ca39762d0a037f635cc
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-07-30 02:36:51 +0200

    style: Fix padding of Product Section

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -9,8 +9,8 @@ import {
 export default function Product({ id }) {
   // Fetch Item Here and send each Details to Components
   return (
-    <div className=" flex flex-col  flex-wrap justify-center gap-2 py-4">
-      <div className="container flex   flex-wrap justify-center gap-2 py-4">
+    <div className=" flex flex-col  flex-wrap justify-center gap-2 ">
+      <div className="container flex   flex-wrap justify-center gap-2 pb-10">
         <ImageSection />
         <ProductDetails />
       </div>
