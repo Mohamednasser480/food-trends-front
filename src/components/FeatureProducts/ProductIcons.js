befba5855797c8b2e5ddf7b8494d2fe2c1fe2366
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-01 01:34:41 +0200

    refact: use New Modal in Products

--- src/components/FeatureProducts/ProductIcons.js
+++ src/components/FeatureProducts/ProductIcons.js
@@ -6,7 +6,7 @@ import "./ProductIcons.css";
 import { Modal } from "../UI";
 
 export default function ProductIcons(props) {
-  const [modalExists, setModalExists] = useState(false);
+  const [isShown, setIsShown] = useState(false);
 
   return (
     <div className={props.className}>
@@ -17,21 +17,15 @@ export default function ProductIcons(props) {
       <ProductIcon
         tooltip="Quick view"
         onClickHandler={() => {
-          setModalExists(true);
+          setIsShown(true);
         }}
       >
         <AiOutlineEye size={25} className={"text-black transition-all"} />
       </ProductIcon>
 
-      {modalExists && (
-        <Modal
-          setModalExists={setModalExists}
-          className="h-1/2 min-w-[970px]   cursor-default bg-white  p-5"
-          effect="flip-down"
-        >
-          <QuickviewProduct />
-        </Modal>
-      )}
+      <Modal show={isShown} setShow={setIsShown}>
+        <QuickviewProduct />
+      </Modal>
 
       <ProductIcon tooltip="Add to Cart">
         <AiOutlineShopping size={25} className={"text-black transition-all"} />
