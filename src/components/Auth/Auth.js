e01ef845452b96d7b4c7b563f34ff2180a223f63
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-03 16:41:28 +0200

    feature: add register modal

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import Login from './Login';
+import Register from './Register';
 
 export default function Auth() {
   const [showRegister, setShowRegister] = useState(false);
 
-  return !showRegister ? <Login setShowRegister={setShowRegister} /> : <div>register</div>;
+  return !showRegister ? <Login setShowRegister={setShowRegister} /> : <Register />;
 }
