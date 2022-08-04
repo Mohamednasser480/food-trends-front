879b001af92c0d7df94586f3c0722bae242c9cd3
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-05 01:48:20 +0200

    bugfix/user-register: Send setShowRegister props property to Register

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,9 +1,13 @@
-import { useState } from 'react';
-import Login from './Login';
-import Register from './Register';
+import { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
 
 export default function Auth() {
   const [showRegister, setShowRegister] = useState(false);
 
-  return !showRegister ? <Login setShowRegister={setShowRegister} /> : <Register />;
+  return !showRegister ? (
+    <Login setShowRegister={setShowRegister} />
+  ) : (
+    <Register setShowRegister={setShowRegister} />
+  );
 }
