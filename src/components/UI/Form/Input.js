964c131ea9c999a823dc0f2d8ec10d6ada6ff0e3
Eslam Ali <preslamali@hotmail.com>
2022-08-04 17:34:21 +0200

    feature/vendor-DB

--- src/components/UI/Form/Input.js
+++ src/components/UI/Form/Input.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Typography } from '../../UI';
+import React from "react";
+import { Typography } from "../../UI";
 
-export default function Input({ register, errors, className, label, id, ...options }) {
-  // console.log(options);
+export default function Input({
+  register,
+  errors,
+  className,
+  label,
+  id,
+  ...options
+}) {
   return (
     <>
       <label htmlFor={id} className="label">
-        <Typography component={'body1'} className="capitalize">
+        <Typography component={"body1"} className="capitalize">
           {label}
         </Typography>
       </label>
@@ -14,7 +20,7 @@ export default function Input({ register, errors, className, label, id, ...optio
         id={id}
         {...register}
         {...options}
-        className={`input input-bordered w-full ${className || ''}`}
+        className={`input input-bordered w-full ${className || ""}`}
       />
       <Typography component="body2" className="h-4 text-red-500">
         {errors[id]?.message}
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer, cartActions } from "./slices/cart";
+import orderReducer from "./Orders/OrdersSlice";
+import productReducer from "./Products/ProductSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    orders: orderReducer,
+    products: productReducer,
   },
 });
 
