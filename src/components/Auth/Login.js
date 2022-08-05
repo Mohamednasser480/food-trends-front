a2ab8c0b12210cacbcc71c15aa7a6acc307bc9e1
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-05 17:38:39 +0200

    feat/user-auth: Dispatch login reducer in Login component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
-import Form from '../UI/Form/Form';
-import { Input } from '../UI/Form';
-import { useForm } from 'react-hook-form';
-import { Button, Typography } from '../UI';
-import { joiResolver } from '@hookform/resolvers/joi';
-import loginSchema from '../../services/form-schemes/login';
+import React from "react";
+import Form from "../UI/Form/Form";
+import { Input } from "../UI/Form";
+import { useForm } from "react-hook-form";
+import { Button, Typography } from "../UI";
+import { joiResolver } from "@hookform/resolvers/joi";
+import loginSchema from "../../services/form-schemes/login";
+import { login } from "../../store/slices/auth";
+import { useDispatch } from "react-redux";
 
 export default function Login({ setShowRegister }) {
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
@@ -16,18 +19,27 @@ export default function Login({ setShowRegister }) {
   });
 
   const loginRegister = {
-    email: { ...register('email') },
-    password: { ...register('password') },
+    email: { ...register("email") },
+    password: { ...register("password") },
   };
 
   return (
-    <Form onSubmit={handleSubmit((d) => console.log(d))} className="p-10">
+    <Form
+      onSubmit={handleSubmit((user) =>
+        dispatch(login({ email: user.email, password: user.password }))
+      )}
+      className="p-10"
+    >
       <Typography component="h1" className="text-center text-primary">
         Sign in
       </Typography>
       <Typography component="body2" className="mt-5 text-center">
         Don't have an account yet?
-        <button type="button" className="mx-1 text-black" onClick={() => setShowRegister(true)}>
+        <button
+          type="button"
+          className="mx-1 text-black"
+          onClick={() => setShowRegister(true)}
+        >
           Sign up
         </button>
         for free
@@ -48,13 +60,16 @@ export default function Login({ setShowRegister }) {
           id="password"
         />
 
-        <button type="button" className="mt-10 self-end font-medium text-black hover:text-primary">
+        <button
+          type="button"
+          className="mt-10 self-end font-medium text-black hover:text-primary"
+        >
           Forgot password?
         </button>
         <Typography component="body2"></Typography>
       </div>
 
-      <Button variant={'secondary'} type="submit" className="mt-10">
+      <Button variant={"secondary"} type="submit" className="mt-10">
         Login
       </Button>
     </Form>
