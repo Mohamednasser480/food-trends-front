6de270f60498fccae4e09e9646c43d4017d9ad3f
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-05 17:31:00 +0200

    feat/user-auth: Create auth slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { cartReducer, cartActions } from "./slices/cart";
+import { cartReducer } from "./slices/cart";
+import { authReducer } from "./slices/auth";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    auth: authReducer,
   },
 });
 
 export default store;
-export { cartActions };
