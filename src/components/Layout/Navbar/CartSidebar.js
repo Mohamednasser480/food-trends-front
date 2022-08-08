c878174562dcd7b86fcc2284734c419c7e53c44d
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-09 01:29:25 +0200

    feat/cart: Add cart products count in user area in navbar

--- src/components/Layout/Navbar/CartSidebar.js
+++ src/components/Layout/Navbar/CartSidebar.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import { AiOutlineShopping } from "react-icons/ai";
 import { Sidebar } from "../../UI";
+import { useSelector } from "react-redux";
+import { selectAllCartItems } from "../../../store/slices/cart";
 
 const CartSidebar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
-
+  const cartProducts = useSelector(selectAllCartItems);
+  const cartProductsCount = cartProducts.reduce(
+    (prevVal, product) => prevVal + product.quantity,
+    0
+  );
   return (
     <>
       <div className="relative">
@@ -16,7 +22,9 @@ const CartSidebar = () => {
             setShowSidebar(true);
           }}
         />
-        <span className="absolute -top-1.5 -right-2 flex h-5  w-5 items-center justify-center rounded-full bg-green-800 text-xs font-bold text-white"></span>
+        <span className="absolute -top-1.5 -right-2 flex h-5  w-5 items-center justify-center rounded-full bg-green-800 text-xs font-bold text-white">
+          {cartProductsCount}
+        </span>
       </div>
       <Sidebar show={showSidebar} setShow={setShowSidebar} right={true}>
         <div>Cart Sidebar</div>
