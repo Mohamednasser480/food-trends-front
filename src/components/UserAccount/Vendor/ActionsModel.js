e3c2616a1dc02fd96ae256de957250f5954a222a
Eslam Ali <preslamali@hotmail.com>
2022-08-08 18:06:11 +0200

    feature/vendor-DB: Add Model to Products comp

---
+++ src/components/UserAccount/Vendor/ActionsModel.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Modal } from "../../UI";
+import AddProduct from "./ActionProductsComponent";
+
+const ActionsModel = ({ actionType, showModel, setShow, ...item }) => {
+  return (
+    <Modal
+      className="h-4/5 w-4/5 overflow-y-auto rounded-lg"
+      show={showModel}
+      setShow={setShow}
+    >
+      <AddProduct actionType={actionType} {...item} />
+    </Modal>
+  );
+};
+
+export default ActionsModel;
