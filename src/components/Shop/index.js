2d52dbfc89a589a8daf1ea866db12ef0d21d3ace
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-09 01:01:00 +0200

    feat: Add title Component of Shop page

---
+++ src/components/Shop/index.js
@@ -0,0 +1,6 @@
+import FiltersSidebar from "./FiltersSidebar";
+import Title from "./Title"
+import Controls from "./Controls"
+import ShopProducts from "./ShopProducts";
+
+export { FiltersSidebar,Title,Controls,ShopProducts };
