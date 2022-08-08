468885c9ce158e0259af7a73947eada69bde2494
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-08 22:56:45 +0200

    feat/cart: Create Spinner component

--- src/components/UI/Loader.js
+++ src/components/UI/Loader.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Spinner } from "./";
 
 const Loader = () => {
   return (
-    <div className="absolute top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-80">
-      <div className="h-12 w-12 animate-spin rounded-full border-[0.45rem] border-dotted border-white"></div>
+    <div className="fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-80">
+      <Spinner />
     </div>
   );
 };
