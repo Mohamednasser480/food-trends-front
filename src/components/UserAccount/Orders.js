a90491b063e469ea0f44e57af499f95fd825ac64
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-08 22:53:01 +0200

    orders filters

--- src/components/UserAccount/Orders.js
+++ src/components/UserAccount/Orders.js
@@ -3,31 +3,37 @@ import OrdersComponent from './OrdersComponent';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchOrders } from '../../store/slices/orders';
 import { Button, Typography } from '../UI';
-import { Select } from '../UI/Form';
+import { selectUserData } from '../../store/slices/auth';
 import { fetchProducts, productsSelector } from '../../store/slices/products';
-import Search from '../UI/Search';
-import Checkbox from '../UI/Form/Checkbox';
 
-const userType = 'customer';
+import Radio from '../UI/Form/Radio';
 
 const filters = ['All Orders', 'Completed', 'Pending', 'Canceled'];
-const selectOptions = [''];
 
 export default function Orders() {
   // const [ordersData, setOrdersData] = useState([]);
 
   const [products, setProducts] = useState([]);
+  const { userType } = useSelector(selectUserData);
 
   const productsData = useSelector(productsSelector);
   const dispatch = useDispatch();
+  const [selected, setSelected] = useState(null);
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setSelected(event.target.value);
+  };
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
-  console.log(productsData);
+  // console.log(productsData);
+
   useEffect(() => {
-    const url = 'https://jsonplaceholder.typicode.com/todos';
+    // const url = 'https://jsonplaceholder.typicode.com/users';
+    const url = 'http://localhost3000/api/v1/orders';
     const fetchData = async () => {
       const response = await fetch(url);
       const json = await response.json();
@@ -37,28 +43,87 @@ export default function Orders() {
     fetchData();
   }, []);
 
+  // useEffect(() => {
+  //   const fetchOrders = async () => {
+  //     const res = await axios.get(
+  //       'http://localhost3000/api/v1/orders?status=pending&sortBy=totalPrice:desc'
+  //     );
+  //     setProducts(res.data);
+  //   };
+
+  //   fetchOrders();
+  // }, []);
+
   useEffect(() => {
     dispatch(fetchOrders());
   }, [dispatch]);
 
-  const onSearch = () => {};
-
   const orders = {
     vendor: () => {
       return (
-        <div className=" container overflow-x-auto">
-          <table className="table-compact table w-full">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Price</th>
-                <th>State</th>
-              </tr>
-            </thead>
-            {<OrdersComponent />}
-          </table>
-        </div>
+        <>
+          <Typography component="h2" className="p-5 text-primary">
+            your orders
+          </Typography>
+
+          <div className="m-5 flex flex-col justify-between xl:w-[1200px] xl:flex-row">
+            <div>
+              {filters.map((filter, index) => {
+                return (
+                  <Button variant="user-account" className="w-32 hover:bg-primary" key={index}>
+                    {filter}
+                  </Button>
+                );
+              })}
+            </div>
+
+            <div className="mx-2 my-5 flex items-center lg:my-0">
+              <Typography component="subtitle2">Price filters:</Typography>
+              <Radio name={'price'} value="highest" checked={selected} onChange={handleChange}>
+                Highest
+              </Radio>
+              <Radio name={'price'} checked={selected} onChange={handleChange} value="lowest">
+                Lowest
+              </Radio>
+            </div>
+          </div>
+
+          {/* {selected === 'highest' ? <div className="bg-red-400">test</div> : ''} */}
+
+          <div className="mx-5 mb-10 flex items-start">
+            <div className="w-full self-center rounded-xl border shadow-lg lg:w-[1200px]">
+              <div className="flex items-center bg-[#f7f7f7] p-2 text-center font-medium text-black">
+                <p className="w-10">Order ID</p>
+                <p className="w-32">Customer</p>
+                <p className="w-32">Order Status</p>
+                <p className="w-32">Order Date</p>
+                <p className="w-32">Delivery Date</p>
+                <p className="w-32">Total Price</p>
+                <p className="w-32">Payment Method</p>
+                <p className="w-32"></p>
+              </div>
+
+              {products.map((rev, index) => {
+                return (
+                  <div className="flex w-full items-center border-b p-3 text-center" key={index}>
+                    <p className="w-10 font-medium">{index + 1}</p>
+                    <p className="w-32 break-words">{rev.name}</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">Not assigned</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">
+                      <Button variant="user-account" className="tracking-tight">
+                        order details
+                      </Button>
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </>
       );
     },
     customer: () => {
@@ -68,41 +133,54 @@ export default function Orders() {
             your orders
           </Typography>
 
-          <div className="m-5 flex">
-            {filters.map((filter) => {
-              return (
-                <Button variant="user-account" className="w-32 hover:bg-primary">
-                  {filter}
-                </Button>
-              );
-            })}
-
+          <div className="m-5 flex flex-col justify-between xl:w-[1200px] xl:flex-row">
             <div>
-              <Checkbox label="cheapest" />
+              {filters.map((filter, index) => {
+                return (
+                  <Button variant="user-account" className="w-32 hover:bg-primary" key={index}>
+                    {filter}
+                  </Button>
+                );
+              })}
+            </div>
+
+            <div className="mx-2 my-5 flex items-center lg:my-0">
+              <Typography component="subtitle2">Price filters:</Typography>
+              <Radio name={'price'} value="highest" checked={selected} onChange={handleChange}>
+                Highest
+              </Radio>
+              <Radio name={'price'} checked={selected} onChange={handleChange} value="lowest">
+                Lowest
+              </Radio>
             </div>
           </div>
 
-          <div className="mx-5 flex items-start">
+          {/* {selected === 'highest' ? <div className="bg-red-400">test</div> : ''} */}
+
+          <div className="mx-5 mb-10 flex items-start">
             <div className="w-full self-center rounded-xl border shadow-lg lg:w-[1200px]">
-              <div className="flex bg-[#f7f7f7] p-2 text-center font-medium text-black">
+              <div className="flex items-center bg-[#f7f7f7] p-2 text-center font-medium text-black">
                 <p className="w-10">Order ID</p>
-                <p className="w-32 break-words">Product</p>
+                <p className="w-32">Product</p>
                 <p className="w-32">Price</p>
                 <p className="w-32">Order Status</p>
-                <p className="mr-5 w-32">Order Date</p>
+                <p className="w-32">Order Date</p>
                 <p className="w-32">Delivery Date</p>
                 <p className="w-32">Payment Method</p>
+                <p className="w-32"></p>
               </div>
 
               {products.map((rev, index) => {
                 return (
-                  <div className="flex w-full items-center border-b p-2 text-center" key={index}>
+                  <div className="flex w-full items-center border-b p-3 text-center" key={index}>
                     <p className="w-10 font-medium">{index + 1}</p>
-                    <p className="w-32 break-words">{rev.title}</p>
+                    <p className="w-32 break-words">{rev.name}</p>
                     <p className="w-32">test</p>
                     <p className="w-32">test</p>
-                    <p className="mr-5 w-32">test</p>
-                    <p className="w-32">
+                    <p className="w-32">test</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">test</p>
+                    <p className="w-48">
                       <Button variant="user-account">order details</Button>
                     </p>
                   </div>
@@ -114,7 +192,71 @@ export default function Orders() {
       );
     },
     delivery: () => {
-      return <div>delivery</div>;
+      return (
+        <>
+          <Typography component="h2" className="p-5 text-primary">
+            your orders
+          </Typography>
+
+          <div className="m-5 flex flex-col justify-between xl:w-[1200px] xl:flex-row">
+            <div>
+              {filters.map((filter, index) => {
+                return (
+                  <Button variant="user-account" className="w-32 hover:bg-primary" key={index}>
+                    {filter}
+                  </Button>
+                );
+              })}
+            </div>
+
+            <div className="mx-2 my-5 flex items-center lg:my-0">
+              <Typography component="subtitle2">Price filters:</Typography>
+              <Radio name={'price'} value="highest" checked={selected} onChange={handleChange}>
+                Highest
+              </Radio>
+              <Radio name={'price'} checked={selected} onChange={handleChange} value="lowest">
+                Lowest
+              </Radio>
+            </div>
+          </div>
+
+          {/* {selected === 'highest' ? <div className="bg-red-400">test</div> : ''} */}
+
+          <div className="mx-5 mb-10 flex items-start">
+            <div className="w-full self-center rounded-xl border shadow-lg lg:w-[1200px]">
+              <div className="flex items-center bg-[#f7f7f7] p-2 text-center font-medium text-black">
+                <p className="w-10">Order ID</p>
+                <p className="w-32">Customer</p>
+                <p className="w-32">Order Status</p>
+                <p className="w-32">Order Date</p>
+                <p className="w-32">Delivery Date</p>
+                <p className="w-32">Total Price</p>
+                <p className="w-32">Payment Method</p>
+                <p className="w-32"></p>
+              </div>
+
+              {products.map((rev, index) => {
+                return (
+                  <div className="flex w-full items-center border-b p-3 text-center" key={index}>
+                    <p className="w-10 font-medium">{index + 1}</p>
+                    <p className="w-32 break-words">{rev.name}</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">Not assigned</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">test</p>
+                    <p className="w-32">
+                      <Button variant="user-account" className="tracking-tight">
+                        order details
+                      </Button>
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      );
     },
   };
 
