396c0e83d7c75875b8e98c677d894097c5055f9c
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-09 17:02:28 +0200

    refact: Change Filters Technique

--- src/components/Shop/Controls.js
+++ src/components/Shop/Controls.js
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Typography } from "../UI";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import FiltersSidebar from "./FiltersSidebar";
+import { FiltersBox, SelectBox } from "./";
 import {
   filteredProductsSelector,
   getFilteredProducts,
 } from "../../store/slices/products";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function Controls() {
+  const [showFilters, setShowFilters] = useState(false);
+
   const { category } = useParams();
+  const navigate = useNavigate();
   const products = useSelector(filteredProductsSelector);
   const dispatch = useDispatch();
   const numberOfProducts = products?.length;
-  // Filters Login
-  const onSelectValue = (e) => {
-    const filterValue = e.target.value;
-    const payload = { number: 5, filter: filterValue, category: category };
+  // Filters Logic
+  let [filtersValues, setFiltersValues] = useState({
+    filterValue: "default",
+    priceValue: "all",
+  });
+  function fetchItemsAfterFilter() {
+    
+    const payload = {
+      number: 5,
+      filter: filtersValues.filterValue,
+      category: category,
+      price: filtersValues.priceValue,
+    };
     dispatch(getFilteredProducts(payload));
+  }
+  const onFilterChange = (e) => {
+    filtersValues.filterValue = e.target.value;
+    console.log(filtersValues)
+    fetchItemsAfterFilter();
+  };
+  const onPriceChange = (e) => {
+    filtersValues.priceValue = e.target.value;
+    fetchItemsAfterFilter();
+  };
+  const onCategoryChange = (e) => {
+    const value = e.target.value;
+    value == "all"
+      ? navigate(`/shop`)
+      : navigate(`/categories/${e.target.value}`);
+    fetchItemsAfterFilter();
   };
   // Filters Array
   const filters = [
@@ -43,36 +72,82 @@ export default function Controls() {
       value: "hightolow",
     },
   ];
-
+  const categories = [
+    {
+      text: "All Categories",
+      value: "all",
+    },
+    {
+      text: "Fruits",
+      value: "fruits",
+    },
+    {
+      text: "Veggies",
+      value: "veggies",
+    },
+    {
+      text: "Meat",
+      value: "meat",
+    },
+    {
+      text: "Dairy",
+      value: "dairy",
+    },
+  ];
+  const prices = [
+    {
+      text: "All Prices",
+      value: "all",
+    },
+    {
+      text: "$0 - $30",
+      value: [0, 30],
+    },
+    {
+      text: "$30 - $60",
+      value: [30, 60],
+    },
+    {
+      text: "$60 - $90",
+      value: [60, 90],
+    },
+    {
+      text: "$90 - $120",
+      value: [90, 120],
+    },
+    {
+      text: "$120 +",
+      value: [120, 99999999],
+    },
+  ];
   return (
-    <div className="mt-6 flex flex-wrap items-center justify-center gap-5 md:justify-between">
-      <Typography
-        component={"subtitle2"}
-        className="text-center normal-case text-primary"
-      >
-        We found
-        <span className="mx-2 text-black">{numberOfProducts}</span>
-        products available for you
-      </Typography>
+    <div className="flex flex-col gap-5">
+      <div className="mt-6 flex flex-wrap items-center justify-center gap-5 md:justify-between">
+        <Typography
+          component={"subtitle2"}
+          className="text-center normal-case text-primary"
+        >
+          We found
+          <span className="mx-2 text-black">{numberOfProducts}</span>
+          products available for you
+        </Typography>
 
-      <div className="flex flex-wrap items-center justify-center gap-5">
-        <div className="relative z-10 flex h-12 w-56 items-center rounded-md  bg-base-200">
-          <select
-            className="border-0.5 peer h-full w-full appearance-none border-0 bg-transparent px-5 font-medium transition-all duration-300 focus:bg-white"
-            onChange={onSelectValue}
+        <div className="flex flex-wrap items-center justify-center gap-5">
+          <SelectBox array={filters} onSelectValue={onFilterChange} />
+          {showFilters && (
+            <>
+              <SelectBox array={categories} onSelectValue={onCategoryChange} />
+              <SelectBox array={prices} onSelectValue={onPriceChange} />
+            </>
+          )}
+
+          <Button
+            variant="primary"
+            onClick={() => setShowFilters(!showFilters)}
           >
-            {filters.map((item) => (
-              <option key={item.value} className="text-lg" value={item.value}>
-                {item.text}
-              </option>
-            ))}
-          </select>
-          <MdOutlineKeyboardArrowDown
-            size={22}
-            className="absolute right-3 -z-10 peer-focus:z-0"
-          />
+            Filters
+          </Button>
         </div>
-        <FiltersSidebar />
       </div>
     </div>
   );
