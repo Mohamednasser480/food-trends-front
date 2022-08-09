539ed36d66191984b405fc94639d3d46e95b5e2e
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-09 22:31:10 +0200

    feat/cart: Create TotalPrice and CartSidebarItem components

---
+++ src/components/Cart/CartSidebarItem.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Typography } from "../UI";
+import { Link } from "react-router-dom";
+import { QuantityInput } from "./index";
+import { HiOutlineX } from "react-icons/hi";
+
+const CartSidebarItem = (props) => {
+  const { cartProduct, onQuantitySubmit, onItemRemove } = props;
+  const { _id, productName, images, price, quantity } = cartProduct;
+
+  return (
+    <li className="flex gap-x-3">
+      <div className="flex items-center">
+        <button
+          onClick={onItemRemove}
+          className="transition-colors hover:text-error"
+        >
+          <HiOutlineX className="h-5 w-5" />
+        </button>
+      </div>
+      <div className="w-24">
+        <img className="max-w-full" src={images[0]} alt={productName} />
+      </div>
+      <section className="flex flex-1 flex-col gap-y-4">
+        <Typography
+          component="subtitle2"
+          className="flex justify-between gap-x-3"
+        >
+          <Link to="/" className="transition-colors hover:text-primary">
+            {productName}
+          </Link>
+          <Typography component="body1">${price.toFixed(2)}</Typography>
+        </Typography>
+        <div className="flex justify-end">
+          <QuantityInput
+            cartProduct={cartProduct}
+            onQuantitySubmit={onQuantitySubmit}
+          />
+        </div>
+      </section>
+    </li>
+  );
+};
+
+export default CartSidebarItem;
--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -3,5 +3,15 @@ import CartList from "./CartList";
 import QuantityInput from "./QuantityInput";
 import CartTableRow from "./CartTableRow";
 import EmptyCart from "./EmptyCart";
+import CartSidebarItem from "./CartSidebarItem";
+import TotalPrice from "./TotalPrice";
 
-export { CartItem, CartList, QuantityInput, CartTableRow, EmptyCart };
+export {
+  CartItem,
+  CartList,
+  QuantityInput,
+  CartTableRow,
+  EmptyCart,
+  CartSidebarItem,
+  TotalPrice,
+};
