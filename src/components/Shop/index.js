9045523b4040318cde0cd2c00cd5e387e11fc688
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-09 17:01:59 +0200

    index

--- src/components/Shop/index.js
+++ src/components/Shop/index.js
@@ -2,6 +2,5 @@ import FiltersSidebar from "./FiltersSidebar";
 import Title from "./Title"
 import Controls from "./Controls"
 import ShopProducts from "./ShopProducts";
-import FiltersBox from "./FiltersBox"
 import SelectBox from "./SelectBox"
 export { FiltersSidebar,Title,Controls,ShopProducts,SelectBox };
