9b776f3cc5315e5b2cc8eef9d673388496b8ed3a
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-09 19:04:19 +0200

    refact: Pass Select Box Ref

--- src/components/Shop/SelectBox.js
+++ src/components/Shop/SelectBox.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
-export default function SelectBox(props) {
+function SelectBox(props, ref) {
   return (
-    <div className="relative z-10 flex h-12 w-56 items-center rounded-md  bg-base-200" data-aos="fade">
+    <div
+      className="relative z-10 flex h-12 w-56 items-center rounded-md  bg-base-200"
+      data-aos="fade"
+    >
       <select
         className="border-0.5 peer h-full w-full appearance-none border-0 bg-transparent px-5 font-medium transition-all duration-300 focus:bg-white"
-        onChange={props.onSelectValue}
+        onChange={props.onChange}
+        ref={ref}
       >
         {props.array.map((item) => (
           <option key={item.value} className="text-lg" value={item.value}>
@@ -21,3 +25,5 @@ export default function SelectBox(props) {
     </div>
   );
 }
+
+export default forwardRef(SelectBox);
