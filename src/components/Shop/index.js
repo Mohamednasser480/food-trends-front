64fd0a8b1ce27b253b14a2719f73371cce1b466e
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-09 17:01:16 +0200

    add Select box to index

--- src/components/Shop/index.js
+++ src/components/Shop/index.js
@@ -4,4 +4,4 @@ import Controls from "./Controls"
 import ShopProducts from "./ShopProducts";
 import FiltersBox from "./FiltersBox"
 import SelectBox from "./SelectBox"
-export { FiltersSidebar,Title,Controls,ShopProducts,FiltersBox,SelectBox };
+export { FiltersSidebar,Title,Controls,ShopProducts,SelectBox };
