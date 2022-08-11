3bc14ef886a674391c5663a232b2db794417953f
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-11 16:50:34 +0200

    refact/layout: Create vendor routes

---
+++ src/routes/vendor/Orders.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Orders = () => {
+  return <div>Vendor Orders</div>;
+};
+
+export default Orders;
---
+++ src/routes/vendor/index.js
@@ -0,0 +1,21 @@
+import Home from "../vendor/Home";
+import ProductDetail from "./ProductDetail";
+import AddProduct from "./AddProduct";
+import Orders from "./Orders";
+import OrderDetail from "./OrderDetail";
+import Reviews from "./Reviews";
+import ReviewDetail from "./ReviewDetail";
+import AccountSettings from "./AccountSettings";
+import Register from "./Register";
+
+export {
+  Home,
+  ProductDetail,
+  AddProduct,
+  Orders,
+  OrderDetail,
+  Reviews,
+  ReviewDetail,
+  AccountSettings,
+  Register,
+};
