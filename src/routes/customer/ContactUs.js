97f4c53a5e094fdcde63bcea1ad0a5a5f00df801
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-11 16:53:22 +0200

    refact/layout: Move customer routes into a new customer folder

---
+++ src/routes/customer/ContactUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { ContactInfo, Info, Map } from "../../components/ContactUs";
+import ContactForm from "../../components/ContactUs/ContactForm";
+import { Breadcrumb } from "../../components/UI";
+
+const ContactUs = () => {
+  return (
+    <>
+      <Breadcrumb />
+      <div className="container flex flex-col py-8">
+        <Info />
+        <ContactInfo />
+        <Map />
+        <ContactForm />
+      </div>
+    </>
+  );
+};
+
+export default ContactUs;
