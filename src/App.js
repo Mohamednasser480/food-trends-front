b20f4cd714b018c77a0cf1e8fa9a7e04a3707567
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-11 16:54:29 +0200

    refact/layout: Refactor Layout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Layout from './components/Layout';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Layout } from "./components/Layout";
 import {
   Home,
   ContactUs,
@@ -12,20 +12,20 @@ import {
   Error404,
   Shop,
   Categories,
-} from './routes';
-import { Routes, Route } from 'react-router-dom';
-import Aos from 'aos';
-import { cookie } from './services';
-import { getUserData } from './store/slices/auth';
-import { fetchCartData } from './store/slices/cart';
-import { selectStatus } from './store/slices/auth';
-import { Wishlist } from './components/UserAccount';
+} from "./routes";
+import { Routes, Route } from "react-router-dom";
+import Aos from "aos";
+import { cookie } from "./services";
+import { getUserData } from "./store/slices/auth";
+import { fetchCartData } from "./store/slices/cart";
+import { selectStatus } from "./store/slices/auth";
+import { Wishlist } from "./components/UserAccount";
 
 function App() {
   const userStatus = useSelector(selectStatus);
   const dispatch = useDispatch();
   useEffect(() => {
-    const token = cookie.getCookie('token');
+    const token = cookie.getCookie("token");
     if (token) {
       dispatch(getUserData(token));
     }
@@ -37,27 +37,28 @@ function App() {
 
   // Initiate Animation
   Aos.init({
-    once: 'true',
+    once: "true",
   });
 
   return (
     <div className="App">
-      <Layout>
-        <Routes>
-          <Route path={'/'} element={<Home />} />
-          <Route path={'/contact-us'} element={<ContactUs />} />
-          <Route path={'/about'} element={<AboutUs />} />
-          <Route path={'/cart'} element={<Cart />} />
-          <Route path={'/user-account'} element={<UserAccount />} />
-          <Route path={'/shop'} element={<Shop />} />
-          <Route path={'/shop/:id'} element={<ProductPage />} />
-          <Route path={'/categories/'} element={<Categories />} />
-          <Route path={'/categories/:category'} element={<Shop />} />
-          <Route path={'/search/:searchText'} element={<SearchPage />} />
-          <Route path={'*'} element={<Error404 />} />
-          <Route path={'/wishlist'} element={<Wishlist />} />
-        </Routes>
-      </Layout>
+      {/*<Layout>*/}
+      {/*  <Routes>*/}
+      {/*    <Route path={"/"} element={<Home />} />*/}
+      {/*    <Route path={"/contact-us"} element={<ContactUs />} />*/}
+      {/*    <Route path={"/about"} element={<AboutUs />} />*/}
+      {/*    <Route path={"/cart"} element={<Cart />} />*/}
+      {/*    <Route path={"/user-account"} element={<UserAccount />} />*/}
+      {/*    <Route path={"/shop"} element={<Shop />} />*/}
+      {/*    <Route path={"/shop/:id"} element={<ProductPage />} />*/}
+      {/*    <Route path={"/categories/"} element={<Categories />} />*/}
+      {/*    <Route path={"/categories/:category"} element={<Shop />} />*/}
+      {/*    <Route path={"/search/:searchText"} element={<SearchPage />} />*/}
+      {/*    <Route path={"*"} element={<Error404 />} />*/}
+      {/*    <Route path={"/wishlist"} element={<Wishlist />} />*/}
+      {/*  </Routes>*/}
+      {/*</Layout>*/}
+      <Layout />
     </div>
   );
 }
