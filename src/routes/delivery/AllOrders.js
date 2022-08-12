a3f30187f8c1e047a067d065e4e7db884710d018
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-12 22:30:56 +0200

    feature: add delivery all order fetch

--- src/routes/delivery/AllOrders.js
+++ src/routes/delivery/AllOrders.js
@@ -1,116 +1,103 @@
 import { useState, useEffect } from 'react';
 import { Button, Typography } from '../../components/UI';
+import { DashboardPage } from '../../components/UI';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUserData, selectUserToken } from '../../store/slices/auth';
 import axios from 'axios';
+import { cookie } from '../../services';
 // import { cookie } from '..';
 
 const AllOrders = () => {
   const [allOrders, setAllOrders] = useState([]);
+  const [orderCount, setOrderCount] = useState(0);
 
-  // const token = cookie.getCookie('token')
-  const token = useSelector(selectUserToken);
+  // const { token } = useSelector(selectUserData);
+  // const token =
+  // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmY1MDVhNmZlMDNiNDM0NDlhZmFlNGIiLCJpYXQiOjE2NjAzMTkzNzksImV4cCI6MTY2MDU3ODU3OX0.QASPSk5qGZm3lSJqloAzlIOKqfkFdiEGiFalHwcuXus';
+  // console.log(token);
 
+  const token = cookie.getCookie('token');
   useEffect(() => {
-    const url = 'https://jsonplaceholder.typicode.com/users';
-    const fetchData = async () => {
-      const response = await fetch(url);
-      const json = await response.json();
-      setAllOrders(json);
-    };
+    const url = process.env.REACT_APP_API_URI + '/delivery';
+    try {
+      const fetchData = async () => {
+        const res = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setAllOrders(res.data.data);
+        setOrderCount(res.data.count);
+      };
 
-    fetchData();
+      fetchData();
+    } catch (e) {
+      console.log(e);
+    }
   }, []);
 
-  // useEffect(() => {
-  //   let url = 'https://jsonplaceholder.typicode.com/users';
-  //  url +=
-  //    selected === 'lowest'
-  //      ? 'sortBy=totalPrice&'
-  //      : selected === 'highest'
-  //      ? 'sortBy=totalPrice:desc&'
-  //      : '';
-  //  if (filterBtn === 'pending') url += 'status=pending&';
-  //  else if (filterBtn === 'canceled') url += 'status=canceled&';
-  //  else if (filterBtn === 'completed') url += 'status=completed&';
+  console.log(allOrders);
 
-  // const token = cookie.getCookie('token');
-  // const fetchData = async () => {
-  //   const response = await axios.get(url);
-  //   {
-  //   headers: { Authorization: 'Bearer ' + token },
-  // });
-
-  // const json = response.data;
-  // setAllOrders(json.data);
-  //   };
-
-  //   fetchData();
-  // }, []);
   return (
-    <>
-      <>
-        <Typography component="h2" className="p-5 text-primary">
-          your orders
-        </Typography>
-
-        <div className="m-5 flex flex-col justify-between xl:w-[1200px] xl:flex-row">
-          <div>
-            {/* {filters.map((filter, index) => {
-                return (
-                  <Button variant="user-account" className="w-32 hover:bg-primary" key={index}>
-                    {filter}
-                  </Button>
-                );
-              })} */}
-          </div>
-          {/* 
-          <div className="mx-2 my-5 flex items-center lg:my-0">
-            <Typography component="subtitle2">Price filters:</Typography>
-            <Radio name={'price'} value="highest" checked={selected} onChange={handleChange}>
-              Highest
-            </Radio>
-            <Radio name={'price'} checked={selected} onChange={handleChange} value="lowest">
-              Lowest
-            </Radio>
-          </div> */}
+    <DashboardPage title="All Orders" className="flex flex-col justify-center">
+      <Typography component="h6" className=" self-end">
+        Your assigned orders: <span className="mt-5 font-medium text-red-400">0</span>
+      </Typography>
+      <Typography component="h6" className="self-end">
+        delivered: <span className="font-medium text-green-400">10</span>
+      </Typography>
+      <Typography component="h6" className="mb-5 self-end">
+        All Orders: <span className="font-medium text-yellow-400">{orderCount}</span>
+      </Typography>
+      <div className="mb-10 flex w-full flex-col items-start px-10">
+        <div className="flex w-full items-center bg-primary p-2 text-center font-medium text-white">
+          <p className="w-1/12 text-lg uppercase">Order ID</p>
+          <p className="w-2/12 text-lg uppercase">Customer Name</p>
+          <p className="w-2/12 text-lg uppercase">Contact</p>
+          <p className="w-2/12 text-lg uppercase">Governorate</p>
+          <p className="w-2/12 text-lg uppercase">City</p>
+          <p className="w-2/12 text-lg uppercase">Order Date</p>
+          <p className="w-2/12 text-lg uppercase">Total Price (EGP)</p>
+          <p className=""></p>
         </div>
 
-        <div className="mx-5 mb-10 flex items-start">
-          <div className="w-full self-center rounded-xl border shadow-lg lg:w-[1200px]">
-            <div className="flex items-center bg-[#f7f7f7] p-2 text-center font-medium text-black">
-              <p className="w-10">Order ID</p>
-              <p className="w-32">Customer</p>
-              <p className="w-32">Governate</p>
-              <p className="w-32">City</p>
-              <p className="w-32">Order Date</p>
-              <p className="w-32">Total Price</p>
-
-              <p className="w-32"></p>
+        {allOrders.map((order, index) => {
+          return (
+            <div className="flex w-full items-center border-b p-3 text-center" key={index}>
+              <Typography component="subtitle2" className="w-1/12 font-medium">
+                {order._id.substring(order._id.length - 6)}
+              </Typography>
+              <Typography component="subtitle2" className="w-2/12 break-words">
+                {order.customer.name}
+              </Typography>
+              <Typography component="subtitle2" className="w-2/12 break-words">
+                {order.customer.mobile}
+              </Typography>
+              <Typography component="subtitle2" className="w-2/12">
+                {order.customer.address.governorate}
+              </Typography>
+              <Typography component="subtitle2" className="w-2/12">
+                {order.customer.address.city}
+              </Typography>
+              <Typography component="subtitle2" className="w-2/12">
+                {order.createdAt.substring(0, 10)}
+              </Typography>
+              <Typography component="subtitle2" className="w-2/12">
+                Not assigned
+              </Typography>
+              <Typography component="subtitle2" className="w-2/12">
+                {order.totalPrice.toFixed(2)}
+              </Typography>
+              <Typography component="subtitle2" className="">
+                <Button variant="secondary" className="tracking-tight">
+                  Assign
+                </Button>
+              </Typography>
             </div>
-
-            {allOrders.map((rev, index) => {
-              return (
-                <div className="flex w-full items-center border-b p-3 text-center" key={index}>
-                  <p className="w-10 font-medium">{index + 1}</p>
-                  {/* <p className="w-32 break-words">{rev.name}</p> */}
-                  <p className="w-32">test</p>
-                  <p className="w-32">test</p>
-                  <p className="w-32">Not assigned</p>
-                  <p className="w-32">test</p>
-                  <p className="w-32">test</p>
-                  <p className="w-32">
-                    <Button variant="secondary" className="tracking-tight">
-                      Assign
-                    </Button>
-                  </p>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </>
-    </>
+          );
+        })}
+      </div>
+    </DashboardPage>
   );
 };
 
