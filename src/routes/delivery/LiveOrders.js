c5caf2b01b07ea6abf83555a4f727cf5f5d6a188
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-12 22:35:05 +0200

    feature: add delivery liveorders

--- src/routes/delivery/LiveOrders.js
+++ src/routes/delivery/LiveOrders.js
@@ -1,5 +1,45 @@
+import { useEffect, useState } from 'react';
+import { DashboardPage } from '../../components/UI';
+
 const LiveOrders = () => {
-  return <></>;
+  const [allOrders, setAllOrders] = useState([]);
+  useEffect(() => {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      setAllOrders(json);
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <DashboardPage title="live orders" className="flex justify-center">
+      <div className="mx-5 mb-10 flex w-full flex-col items-start">
+        <div className="flex w-full items-center bg-primary p-2 text-center font-medium text-white">
+          <p className="w-1/12 text-lg uppercase">Order ID</p>
+          <p className="w-2/12 text-lg uppercase">Customer</p>
+          <p className="w-2/12 text-lg uppercase">Governate</p>
+          <p className="w-2/12 text-lg uppercase">City</p>
+          <p className="w-2/12 text-lg uppercase">Order Date</p>
+          <p className="w-2/12 text-lg uppercase">Total Price</p>
+        </div>
+
+        {allOrders.map((rev, index) => {
+          return (
+            <div className="flex w-full items-center border-b p-3 text-center" key={index}>
+              <p className="w-1/12 font-medium">{index + 1}</p>
+              <p className="w-2/12 break-words">{rev.name}</p>
+              <p className="w-2/12">test</p>
+              <p className="w-2/12">test</p>
+              <p className="w-2/12">Not assigned</p>
+              <p className="w-2/12">test</p>
+            </div>
+          );
+        })}
+      </div>
+    </DashboardPage>
+  );
 };
 
 export default LiveOrders;
