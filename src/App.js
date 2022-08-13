ff81e7eb1170bd5f2ad8a2fbe88475fd2512c386
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-08-13 09:05:49 +0200

    bugfix/loading-data: Fetch user data before rendering layout

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Layout } from "./components/Layout";
 import Aos from "aos";
 import { cookie } from "./services";
 import { getUserData } from "./store/slices/auth";
-import { fetchCartData } from "./store/slices/cart";
 import { selectStatus } from "./store/slices/auth";
+import { Loader } from "./components/UI";
 // import { Wishlist } from "./components/UserAccount";
 
 function App() {
-  const userStatus = useSelector(selectStatus);
   const dispatch = useDispatch();
+  const userStatus = useSelector(selectStatus);
+
   useEffect(() => {
     const token = cookie.getCookie("token");
     if (token) {
@@ -25,23 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      {/*<Layout>*/}
-      {/*  <Routes>*/}
-      {/*    <Route path={"/"} element={<Home />} />*/}
-      {/*    <Route path={"/contact-us"} element={<ContactUs />} />*/}
-      {/*    <Route path={"/about"} element={<AboutUs />} />*/}
-      {/*    <Route path={"/cart"} element={<Cart />} />*/}
-      {/*    <Route path={"/user-account"} element={<UserAccount />} />*/}
-      {/*    <Route path={"/shop"} element={<Shop />} />*/}
-      {/*    <Route path={"/shop/:id"} element={<ProductPage />} />*/}
-      {/*    <Route path={"/categories/"} element={<Categories />} />*/}
-      {/*    <Route path={"/categories/:category"} element={<Shop />} />*/}
-      {/*    <Route path={"/search/:searchText"} element={<SearchPage />} />*/}
-      {/*    <Route path={"*"} element={<Error404 />} />*/}
-      {/*    <Route path={"/wishlist"} element={<Wishlist />} />*/}
-      {/*  </Routes>*/}
-      {/*</Layout>*/}
-      <Layout />
+      {userStatus === "loading" ? <Loader /> : <Layout />}
     </div>
   );
 }
