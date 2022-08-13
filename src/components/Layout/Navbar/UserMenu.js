b4c40fffa60a9317e1f7ee059ef1c3925f8c95ad
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-14 01:45:07 +0200

    Edit User Menu

--- src/components/Layout/Navbar/UserMenu.js
+++ src/components/Layout/Navbar/UserMenu.js
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 const UserMenu = (props) => {
   const menuItems = {
     vendor: ["Dashboard"],
-    customer: ["My profile", "My orders", "Reviews"],
+    customer: ["My profile"],
     delivery: ["Orders"],
   };
   const dispatch = useDispatch();
@@ -25,7 +25,7 @@ const UserMenu = (props) => {
         return (
           <Fragment key={index}>
             <li className="capitalize transition-colors hover:bg-black hover:text-white">
-              <Link to={"/user-account"}>{item}</Link>
+              <Link to={`${userType=="customer"?"/user-account":"/"}`}>{item}</Link>
             </li>
           </Fragment>
         );
