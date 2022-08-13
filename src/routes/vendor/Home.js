f9c540a09f25e9351d0ff518ae5209cb9e0d4ed4
Eslam Ali <preslamali@hotmail.com>
2022-08-13 16:01:25 +0200

    bugfix/vendor-DB: remove forget password

--- src/routes/vendor/Home.js
+++ src/routes/vendor/Home.js
@@ -48,9 +48,9 @@ const Home = () => {
         <Typography component="body2">{mobile}</Typography>
         <Typography component="body2">{email}</Typography>
         {/* <Typography component="body2">address</Typography> */}
-        <Button variant="secondary" className="my-2 self-center">
+        {/* <Button variant="secondary" className="my-2 self-center">
           Edit profile
-        </Button>
+        </Button> */}
       </div>
       <div className="flex w-full gap-x-5">
         <div className="flex w-full flex-col rounded-xl bg-white p-5 shadow-md">
@@ -69,7 +69,7 @@ const Home = () => {
             See All Statistics
           </Button>
         </div>
-        <div className="flex w-full flex-col rounded-xl bg-white p-5 shadow-md">
+        {/* <div className="flex w-full flex-col rounded-xl bg-white p-5 shadow-md">
           <Typography component="h4" className="text-primary">
             change password
           </Typography>
@@ -103,7 +103,7 @@ const Home = () => {
               Submit
             </Button>
           </Form>
-        </div>
+        </div> */}
       </div>
     </div>
   );
