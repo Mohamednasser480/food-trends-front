b8f81ed461059021ae77069891ca37318d479327
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-13 22:57:15 +0200

    Add Speical Offer to Cart

--- src/components/SpecialOffer/SpecialOfferSection.js
+++ src/components/SpecialOffer/SpecialOfferSection.js
@@ -2,17 +2,38 @@ import React from "react";
 import { CountDwon } from "./";
 import { Button, Typography } from "../UI";
 import { useCountdown } from "./CounterHook";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  saveCartItem,
+  selectAllCartItems,
+  selectStatus,
+} from "../../store/slices/cart";
 const SpecialOfferSection = () => {
   const item = {
-    title: "FRESH FRUIT BOX",
-    img: `${require("../../assets/SpecialOffer1.png")}`,
+    _id: "SpecialOffer#000000100001",
+    productName: "FRESH FRUIT BOX",
+    images: [`${require("../../assets/SpecialOffer1.png")}`],
     offerImg: `${require("../../assets/SpecialOffer2.png")}`,
-    offerExpier: "2022-08-15T14:11:59.604Z",
+    offerExpier: "2022-08-19T14:11:59.604Z",
+    _id: "62f7a8ee638f98104c72294e",
+    summary: "FRESH FRUIT BOX",
+    description:
+      "Organic foods are the only source which we can rely on to take proper health care. Organic food is the most nutritious food and should be consumed by everyone.",
+    weight: 10,
+    category: "fruits",
+    price: 29.0,
+    inStock: 200,
+    discount: 0,
+    rate: 0,
+    numberOfReviews: 0,
+    vendor: "62f66dcf7ac0be1f0dd16d6e",
   };
-
   const [days, hours, mins, secs] = useCountdown(item.offerExpier);
-
+  const dispatch = useDispatch();
+  const cartProducts = useSelector(selectAllCartItems);
+  const productQuantity =
+    cartProducts.find((cartProduct) => cartProduct._id === item._id)
+      ?.quantity || 0;
   return (
     <div
       className="flex min-h-[600px] flex-wrap bg-[#F0EBD8] py-4 px-0 lg:px-20"
@@ -24,10 +45,10 @@ const SpecialOfferSection = () => {
           data-aos="fade-up"
           data-aos-delay="300"
         >
-          <img src={item.img} alt="..." className="md:mx-auto" />
+          <img src={item?.images[0]} alt="..." className="md:mx-auto" />
           <img
             className=" absolute left-10 top-14 m-2 w-24 lg:w-32 "
-            alt={item.title}
+            alt={item.productName}
             src={item.offerImg}
           />
         </div>
@@ -63,7 +84,14 @@ const SpecialOfferSection = () => {
           <Button
             variant="secondary"
             className="my-5 w-4/6 !tracking-normal text-white transition-all duration-700 hover:bg-[#3D6642] lg:w-6/12"
-            to={"/shop"}
+            onClick={() => {
+              dispatch(
+                saveCartItem({
+                  ...item,
+                  quantity: productQuantity + 1,
+                })
+              );
+            }}
           >
             Add To Bag
           </Button>
