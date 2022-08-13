e3f8ff55fb7aca8c3676fff234b4c95ddf651b96
Eslam Ali <preslamali@hotmail.com>
2022-08-13 16:01:04 +0200

    bugfix/vendor-DB: bugfix

--- src/routes/vendor/AddProduct.js
+++ src/routes/vendor/AddProduct.js
@@ -33,12 +33,12 @@ const AddProduct = ({
   const [images, setImages] = useState(null);
 
   const selectOptions = [
-    "Dairy",
-    "Fruits",
-    "Bakery",
-    "Meat",
-    "Veggies",
-    "Chicken",
+    "dairy",
+    "fruits",
+    "bakery",
+    "meat",
+    "veggies",
+    "chicken",
   ];
 
   const {
@@ -93,7 +93,6 @@ const AddProduct = ({
     formData.append("discount", e.discount);
     formData.append("weight", e.weight);
 
-    console.log(actionType);
     if (actionType === "EDIT") {
       dispatch(updateProduct({ _id, formData }));
     } else dispatch(addProduct(formData));
@@ -102,134 +101,132 @@ const AddProduct = ({
   };
 
   return (
-    <div>
-      <div className="bg-[#f8f9fa] p-10">
-        {vendorStatus === " Pending" ? <Loader /> : null}
-        <Typography
-          component={"h2"}
-          className="mb-10 tracking-tight text-primary"
-        >
-          {actionType === "EDIT" ? "Edit the Product" : "Add New Product"}
-        </Typography>
-        <Form onSubmit={handleSubmit((e) => handleProduct(e))}>
-          <div className="flex flex-col lg:flex-row">
-            <div className="mr-9 flex w-full flex-col rounded-xl bg-white p-10 lg:w-2/3">
-              <div className="flex w-2/3 justify-between">
-                <div>
-                  <Input
-                    register={addProductRegister.productName}
-                    errors={errors}
-                    type="text"
-                    placeholder="Type here"
-                    label="product name"
-                    id="productName"
-                  />
-                </div>
-
-                <div className="w-1/2">
-                  <Input
-                    register={addProductRegister.summary}
-                    errors={errors}
-                    type="text"
-                    placeholder="Type here"
-                    label="summary"
-                    id="summary"
-                  />
-                </div>
+    <div className="bg-[#f8f9fa] p-10">
+      {vendorStatus === " Pending" ? <Loader /> : null}
+      <Typography
+        component={"h2"}
+        className="mb-10 tracking-tight text-primary"
+      >
+        {actionType === "EDIT" ? "Edit the Product" : "Add New Product"}
+      </Typography>
+      <Form onSubmit={handleSubmit((e) => handleProduct(e))}>
+        <div className="flex flex-col">
+          <div className="mr-9 flex w-full flex-col rounded-xl bg-white p-10">
+            <div className="flex justify-between gap-4">
+              <div className=" w-1/2">
+                <Input
+                  register={addProductRegister.productName}
+                  errors={errors}
+                  type="text"
+                  placeholder="Type here"
+                  label="product name"
+                  id="productName"
+                />
               </div>
-              <TextArea
-                register={addProductRegister.description}
-                label="Full description"
-                errors={errors}
-                placeholder="Full description"
-                id="description"
-              />
 
-              <div className="md:grid md:grid-cols-3 md:gap-4">
-                <div className="form-control w-full max-w-xs">
-                  <Input
-                    register={addProductRegister.price}
-                    errors={errors}
-                    type="number"
-                    placeholder="LE"
-                    label="regular price"
-                    id="price"
-                  />
-                </div>
-                <div className="form-control">
-                  <Input
-                    register={addProductRegister.weight}
-                    errors={errors}
-                    type="number"
-                    placeholder="Kg"
-                    label="Weight"
-                    id="weight"
-                  />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                  <Input
-                    register={addProductRegister.discount}
-                    errors={errors}
-                    type="number"
-                    placeholder="LE"
-                    label="discount"
-                    id="discount"
-                  />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                  <Input
-                    register={addProductRegister.inStock}
-                    errors={errors}
-                    type="number"
-                    placeholder="items"
-                    label="stock"
-                    id="inStock"
-                  />
-                </div>
+              <div className="w-1/2">
+                <Input
+                  register={addProductRegister.summary}
+                  errors={errors}
+                  type="text"
+                  placeholder="Type here"
+                  label="summary"
+                  id="summary"
+                />
               </div>
             </div>
-            <div className="mt-10 w-full rounded-xl bg-white p-10 lg:mt-0 lg:w-1/4">
-              <Typography component="h5">Media</Typography>
-              <DragAndDrop label="Product Images" onAddImg={addImageHandler} />
-              <div className="container m-4 flex">
-                {images && (
-                  <img
-                    className="h-32 w-32 "
-                    src={URL.createObjectURL(images)}
-                    alt="..."
-                  />
-                )}
-              </div>
-              <div className="form-control mt-5 w-full">
-                <Select
-                  selectOptions={selectOptions}
+            <TextArea
+              register={addProductRegister.description}
+              label="Full description"
+              errors={errors}
+              placeholder="Full description"
+              id="description"
+            />
+
+            <div className="md:grid md:grid-cols-3 md:gap-4">
+              <div className="form-control w-full max-w-xs">
+                <Input
+                  register={addProductRegister.price}
                   errors={errors}
-                  register={addProductRegister.category}
-                  label="category"
-                  id="category"
+                  type="number"
+                  placeholder="LE"
+                  label="regular price"
+                  id="price"
+                />
+              </div>
+              <div className="form-control">
+                <Input
+                  register={addProductRegister.weight}
+                  errors={errors}
+                  type="number"
+                  placeholder="Kg"
+                  label="Weight"
+                  id="weight"
+                />
+              </div>
+              <div className="form-control w-full max-w-xs">
+                <Input
+                  register={addProductRegister.discount}
+                  errors={errors}
+                  type="number"
+                  placeholder="LE"
+                  label="discount"
+                  id="discount"
+                />
+              </div>
+              <div className="form-control w-full max-w-xs">
+                <Input
+                  register={addProductRegister.inStock}
+                  errors={errors}
+                  type="number"
+                  placeholder="items"
+                  label="stock"
+                  id="inStock"
                 />
               </div>
             </div>
           </div>
-          {actionType === "EDIT" ? (
-            <Button
-              variant="primary"
-              type="submit"
-              className="mt-20 w-1/2 self-center lg:w-2/12 lg:self-start"
-            >
-              Update
-            </Button>
-          ) : (
-            <Button
-              variant="secondary"
-              type="submit"
-              className="mt-20 w-1/2 self-center lg:w-2/12 lg:self-start"
-            >
-              Add
-            </Button>
-          )}
-        </Form>
-      </div>
+          <div className=" mt-10 w-full rounded-xl bg-white p-10 ">
+            <Typography component="h5">Media</Typography>
+            <DragAndDrop label="Product Images" onAddImg={addImageHandler} />
+            <div className="container m-4 flex">
+              {images && (
+                <img
+                  className="h-32 w-32 "
+                  src={URL.createObjectURL(images)}
+                  alt="..."
+                />
+              )}
+            </div>
+            <div className="form-control mt-5 w-full">
+              <Select
+                selectOptions={selectOptions}
+                errors={errors}
+                register={addProductRegister.category}
+                label="category"
+                id="category"
+              />
+            </div>
+          </div>
+        </div>
+        {actionType === "EDIT" ? (
+          <Button
+            variant="primary"
+            type="submit"
+            className="mt-20 w-1/2 self-center lg:self-start"
+          >
+            Update
+          </Button>
+        ) : (
+          <Button
+            variant="secondary"
+            type="submit"
+            className="mt-20 w-1/2 self-center "
+          >
+            Add
+          </Button>
+        )}
+      </Form>
     </div>
   );
 };
