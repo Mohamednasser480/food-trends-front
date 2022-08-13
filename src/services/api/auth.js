9b60c1d9085ef6cfef764f6cf01b0da5abb9ad90
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-13 23:51:33 +0200

    Add address to Vendor Register

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -73,6 +73,10 @@ const registerVendor = async (newUser) => {
       mobile: "01279001036",
       storeName: newUser.storeName,
       userType: "vendor",
+      address: {
+        city: "any",
+        governorate: "any",
+      },
     });
 
     return res.data;
