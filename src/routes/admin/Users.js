3eb9695a40b7a469ff2060b1360c22b7d4ef74e9
mahmoudmeky <mahmoudmeky2017@gmail.com>
2022-08-15 22:05:04 +0200

    Create Users Page

--- src/routes/admin/Users.js
+++ src/routes/admin/Users.js
@@ -1,79 +1,130 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import {
+  adminUsersSelector,
+  getUsers,
+  paginateUsers,
+} from "../../store/slices/admin";
+import { useDispatch, useSelector } from "react-redux";
+import { Pagination } from "../../components/UI";
 export default function Users() {
-
+  const dispatch = useDispatch();
+  const users = useSelector(adminUsersSelector).users;
+  const usersCount = useSelector(adminUsersSelector).count;
+  const currentPage = useSelector(adminUsersSelector).currentPage;
+  useEffect(() => {
+    dispatch(getUsers());
+  }, []);
+  const changeUsersPerPage = (pageNumber) => {
+    dispatch(paginateUsers({ pageNumber: pageNumber }));
+  };
   return (
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table class="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-        <thead class="bg-primary text-xs uppercase text-white  dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" class="py-3 px-4">Image</th>
-            <th scope="col" class="py-3 px-6">
-              Username
-            </th>
-            <th scope="col" class="py-3 px-6">
-              StoreName
-            </th>
-            <th scope="col" class="py-3 px-6">
-              Email
-            </th>
-            <th scope="col" class="py-3 px-6">
-              Mobile Number
-            </th>
-            <th scope="col" class="py-3 px-6">
-              Address
-            </th>
-            <th scope="col" class="py-3 px-6">
-              User Type
-            </th>
-            <th scope="col" class="py-3 px-6">
-              Status
-            </th>
-            <th scope="col" class="py-3 px-6">
-              <span class="sr-only">Edit</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-            <td class="py-4 px-4">
-              <img
-                src="https://pps.whatsapp.net/v/t61.24694-24/298927329_581478700281260_2120462059725420128_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AVx8deUAFoSD5mOSTFBQIsvnafaHZqFagMofuLHVwfR7gg&oe=6308CA9B"
-                className="h-12 w-12 rounded-full object-cover"
-              />
-            </td>
+    <section className="">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+          <thead className="bg-primary text-xs uppercase text-white  dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="py-3 px-4">
+                Image
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Username
+              </th>
+              <th scope="col" className="py-3 px-6">
+                StoreName
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Email
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Mobile Number
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Address
+              </th>
+              <th scope="col" className="py-3 px-6">
+                User Type
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Status
+              </th>
+              <th scope="col" className="py-3 px-6">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users &&
+              users.map((user, index) => {
+                return (
+                  <tr
+                    key={index}
+                    className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+                  >
+                    <td className="py-4 px-4">
+                      <img
+                        src={user?.image}
+                        className="h-12 w-12 rounded-full object-cover"
+                      />
+                    </td>
 
-            <td class="py-4 px-6">Username</td>
-            <td class="py-4 px-6">Storename</td>
-            <th
-              scope="row"
-              class="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white"
-            >
-              Email@email.com
-            </th>
-            <td class="py-4 px-6">01010595485</td>
-            <td class="py-4 px-6">Imbaba - Giza</td>
-            <td class="py-4 px-6">Admin</td>
-            <td class="py-4 px-6">Pending</td>
-            <td class=" py-4 px-1">
-              <div className="flex items-center justify-center gap-2">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Activate
-                </a>
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Delete
-                </a>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+                    <td className="py-4 px-6 capitalize">{user?.name}</td>
+                    <td className="py-4 px-6 capitalize">{user?.storeName}</td>
+                    <th
+                      scope="row"
+                      className="whitespace-nowrap py-4 px-2 font-medium text-gray-900 dark:text-white"
+                    >
+                      {user?.email}
+                    </th>
+                    <td className="py-4 px-2">{user?.mobile}</td>
+                    <td className="py-4 px-2 capitalize">{`${user?.address?.governorate} - ${user?.address?.city} `}</td>
+                    <td className="py-4 px-6 capitalize">{user?.userType}</td>
+                    <td
+                      className={`py-4 px-6 font-bold capitalize ${
+                        user?.verified == "pending"
+                          ? "text-orange-400 "
+                          : user?.verified == "true"
+                          ? "text-green-500"
+                          : "text-red-500"
+                      }`}
+                    >
+                      {user?.verified == "true"
+                        ? "Verified"
+                        : user?.verified == "false"
+                        ? "Refused"
+                        : "Pending"}
+                    </td>
+                    <td className=" py-4 px-1">
+                      <div className="flex flex-col items-center  justify-center gap-2 px-3 ">
+                        {(user?.verified == "false" ||
+                          user?.verified == "pending") &&
+                          user?.userType !== "customer" && (
+                            <a
+                              href="#"
+                              className="rounded-lg bg-green-500  py-1 px-2 font-bold text-white transition-all  duration-300 hover:bg-green-700 dark:text-blue-500"
+                            >
+                              Verify
+                            </a>
+                          )}
+                        <a
+                          href="#"
+                          className="rounded-lg bg-red-500  py-1 px-2 font-bold text-white transition-all duration-300 hover:bg-red-700 dark:text-blue-500"
+                        >
+                          Delete
+                        </a>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+      <Pagination
+        onPageChange={changeUsersPerPage}
+        currentPage={currentPage}
+        numberOfItems={usersCount}
+        numberOfItemsToShow={8}
+      />
+    </section>
   );
 }
