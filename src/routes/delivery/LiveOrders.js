263aa32b83b99d92f4b805f791c700714cbd7c38
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-16 14:52:28 +0200

    refactor: add deassign feature

--- src/routes/delivery/LiveOrders.js
+++ src/routes/delivery/LiveOrders.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { DashboardPage, Typography } from '../../components/UI';
-import { cookie } from '../../services';
+import { Button, DashboardPage, Typography } from '../../components/UI';
 import {
   assignedOrdersCountSelector,
   assignedOrdersSelector,
+  deassignOrder,
   getAssignedOrders,
+  setOrderComplete,
 } from '../../store/slices/delivery';
 
 const LiveOrders = () => {
@@ -19,77 +19,68 @@ const LiveOrders = () => {
     dispatch(getAssignedOrders());
   }, []);
 
-  console.log(data);
-  // useEffect(() => {
-  //   const url = process.env.REACT_APP_API_URI + '/delivery/me?status=assigned';
-  //   try {
-  //     const fetchData = async () => {
-  //       const res = await axios.get(url, {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       });
-  //       console.log(res.data);
-  //       setAssignedOrders(res.data);
-  //     };
-
-  //     fetchData();
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // }, []);
-
-  console.log(data);
-  // useEffect(() => {
-  //   const url = process.env.REACT_APP_API_URI + '/delivery/me?status=assigned';
-  //   try {
-  //     const fetchData = async () => {
-  //       const res = await axios.get(url, {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       });
-  //       console.log(res.data);
-  //       setAssignedOrders(res.data);
-  //     };
-
-  //     fetchData();
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // }, []);
+  const handleSetComplete = (id) => {
+    dispatch(setOrderComplete(id));
+  };
+  const handleDeassign = (id) => {
+    dispatch(deassignOrder(id));
+  };
 
   return (
-    <DashboardPage title="live orders" className="flex justify-center">
+    <DashboardPage title="your orders" className="flex justify-center">
       <div className="mx-5 mb-10 flex w-full flex-col items-start">
-        <div className="flex w-full items-center bg-primary p-2 text-center font-medium text-white">
-          <p className="w-1/12 text-lg uppercase">Order ID</p>
-          <p className="w-2/12 text-lg uppercase">Customer Name</p>
-          <p className="w-2/12 text-lg uppercase">Contact</p>
-          <p className="w-2/12 text-lg uppercase">Governorate</p>
-          <p className="w-2/12 text-lg uppercase">City</p>
-          <p className="w-2/12 text-lg uppercase">Order Date</p>
-          <p className="w-2/12 text-lg uppercase">Total Price (EGP)</p>
+        <div className="flex w-full items-center bg-primary p-2 text-center font-medium text-white [&>*]:w-2/12 [&>*]:text-lg [&>*]:uppercase">
+          <p className="w-14">Order ID</p>
+          <p>Customer Name</p>
+          <p>Contact</p>
+          <p>Governorate</p>
+          <p>City</p>
+          <p>Order Date</p>
+          <p>Total Price (EGP)</p>
+          <p></p>
         </div>
 
         {data.map((order, index) => {
           return (
-            <div className="flex w-full items-center border-b p-3 text-center" key={index}>
+            <div
+              className="[&>*]:text-md flex w-full items-center border-b py-1 text-center [&>*]:w-2/12"
+              key={index}
+            >
               <Typography component="subtitle2" className="w-1/12 font-medium">
                 {order._id.substring(order._id.length - 6)}
               </Typography>
-              <Typography component="subtitle2" className="w-2/12 break-words">
-                {order.customer.name}
-              </Typography>
-              <Typography component="subtitle2" className="w-2/12 break-words">
-                {order.customer.mobile}
-              </Typography>
-              <Typography component="subtitle2" className="w-2/12">
-                {order.customer.address.governorate}
-              </Typography>
-              <Typography component="subtitle2" className="w-2/12">
-                {order.customer.address.city}
-              </Typography>
+              {order.customer === null ? (
+                <>
+                  <Typography component="subtitle2" className="w-2/12 break-words">
+                    Deleted User
+                  </Typography>
+                  <Typography component="subtitle2" className="w-2/12 break-words">
+                    Deleted User
+                  </Typography>
+                  <Typography component="subtitle2" className="w-2/12">
+                    Deleted User
+                  </Typography>
+                  <Typography component="subtitle2" className="w-2/12">
+                    Deleted User
+                  </Typography>
+                </>
+              ) : (
+                <>
+                  <Typography component="subtitle2" className="w-2/12 break-words">
+                    {order.customer.name}
+                  </Typography>
+                  <Typography component="subtitle2" className="w-2/12 break-words">
+                    {order.customer.mobile}
+                  </Typography>
+                  <Typography component="subtitle2" className="w-2/12">
+                    {order.customer.address.governorate}
+                  </Typography>
+                  <Typography component="subtitle2" className="w-2/12">
+                    {order.customer.address.city}
+                  </Typography>
+                </>
+              )}
+
               <Typography component="subtitle2" className="w-2/12">
                 {order.createdAt.substring(0, 10)}
               </Typography>
@@ -97,6 +88,20 @@ const LiveOrders = () => {
               <Typography component="subtitle2" className="w-2/12">
                 {order.totalPrice.toFixed(2)}
               </Typography>
+              <div>
+                <button
+                  className="rounded-lg bg-red-500 p-1 uppercase text-white"
+                  onClick={() => handleDeassign(order._id)}
+                >
+                  remove
+                </button>
+                <button
+                  className="rounded-lg bg-green-500 p-1 uppercase text-white"
+                  onClick={() => handleSetComplete(order._id)}
+                >
+                  complete
+                </button>
+              </div>
             </div>
           );
         })}
