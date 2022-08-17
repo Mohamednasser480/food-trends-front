70bfb764b82230046aeac48c056862f449521a98
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-17 23:51:11 +0200

    fix

--- src/components/ContactUs/Map.js
+++ src/components/ContactUs/Map.js
@@ -16,7 +16,7 @@ const Map = () => {
       >
         <TileLayer
           attribution="© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
-          url="  https://api.mapbox.com/styles/v1/eslamali/cl5phz2nm007315pcsz1y7sid/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXNsYW1hbGkiLCJhIjoiY2w1cGhoMnZqMW40cTNibWdkdnJpemV1ciJ9.TATXx-VuxUYiwRvapGEK6A"
+          url="https://api.mapbox.com/styles/v1/eslamali/cl5phz2nm007315pcsz1y7sid/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXNsYW1hbGkiLCJhIjoiY2w1cGhoMnZqMW40cTNibWdkdnJpemV1ciJ9.TATXx-VuxUYiwRvapGEK6A"
         />
         <Marker position={position}>
           <Popup>ITI Menofia</Popup>
