3af7a19378887d33843a2be7c86f741046b6d2ee
MuhEid96 <moh.eid.1996@gmail.com>
2022-08-17 22:36:58 +0200

    refactor: navigate user to home when logout

--- src/components/Layout/Navbar/UserMenu.js
+++ src/components/Layout/Navbar/UserMenu.js
@@ -1,44 +1,46 @@
-import React, { Fragment } from "react";
-import { useDispatch } from "react-redux";
-import { logout } from "../../../store/slices/auth";
-import { useSelector } from "react-redux";
-import { selectUserData } from "../../../store/slices/auth";
-import { Link } from "react-router-dom";
+import React, { Fragment } from 'react';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../store/slices/auth';
+import { useSelector } from 'react-redux';
+import { selectUserData } from '../../../store/slices/auth';
+import { Link, useNavigate } from 'react-router-dom';
 
 const UserMenu = (props) => {
   const menuItems = {
-    vendor: ["Dashboard"],
-    customer: ["My profile"],
-    delivery: ["Orders"],
-    admin:[]
+    vendor: ['Dashboard'],
+    customer: ['My profile'],
+    delivery: ['Orders'],
+    admin: [],
   };
+
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const logoutHandler = () => dispatch(logout());
+  const logoutHandler = () => {
+    dispatch(logout());
+    navigate('/');
+  };
 
   const { userType } = useSelector(selectUserData);
 
   return (
     <ul
       onClick={props.onClick}
-      className="menu rounded-box absolute top-12 right-0 w-48 bg-white shadow-md z-10"
+      className="menu rounded-box absolute top-12 right-0 z-10 w-48 bg-white shadow-md"
     >
       {menuItems[userType].map((item, index) => {
         return (
           <Fragment key={index}>
             <li className="capitalize transition-colors hover:bg-black hover:text-white">
-              <Link to={`${userType=="customer"?"/user-account":"/"}`}>{item}</Link>
+              <Link to={`${userType == 'customer' ? '/user-account' : '/'}`}>{item}</Link>
             </li>
           </Fragment>
         );
       })}
-      {userType == "customer" ? (
+      {userType == 'customer' ? (
         <>
           <li>
-            <Link to={"/cart"}>My cart</Link>
+            <Link to={'/cart'}>My cart</Link>
           </li>
-          {/* <li>
-            <Link to={"/wishlist"}>My Wishlist</Link>
-          </li> */}
         </>
       ) : null}
 
